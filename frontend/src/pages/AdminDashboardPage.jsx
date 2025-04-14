fb141a5249d4c1358d01d2a6d453bbfa710578d6
swati jain <swati21work@gmail.com>
2025-04-15 02:12:17 +0530

    Update AdminDashboardPage.jsx

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://friendbook-6x6d.onrender.com';
 
 const AdminDashboardPage = () => {
   const [users, setUsers] = useState([]);
