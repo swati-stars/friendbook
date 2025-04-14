8ab5a00cf6df0fc2b9dfd0d8bb727e8c974a0211
swati jain <swati21work@gmail.com>
2025-04-15 03:01:21 +0530

    Update UserProfilePage.jsx

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://friendbook-6x6d.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://friendbook-dx9o.onrender.com';
 
 const UserProfilePage = () => {
   const [userData, setUserData] = useState(null);
