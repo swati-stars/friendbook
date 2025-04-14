9c4d6512c3177732d5d2cec44e5d9abf5b89c6be
swati jain <swati21work@gmail.com>
2025-04-15 02:11:36 +0530

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || http://friendbook-6x6d.onrender.com';
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({ username: '', password: '' });
